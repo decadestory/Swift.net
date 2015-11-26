0eec9d626c1bd776f33ae9fcb85f97dd7271bb18
decadestory <728246447@qq.com>
2015-11-26 18:36:55 +0800

    baseentity

--- Swift.Net/Base/BaseEntity.cs
+++ Swift.Net/Base/BaseEntity.cs
@@ -9,10 +9,16 @@ namespace Swift.Net.Base
 {
     public class BaseEntity
     {
+        public BaseEntity()
+        {
+            Enable = true;
+            AddTime=DateTime.Now;
+            EditTime=DateTime.Now;
+        }
+
         public DateTime? AddTime { get; set; }
         public DateTime? EditTime { get; set; }
         public string Remark { get; set; }
-        [DefaultValue(true)]
-        public bool Enable { get; set; }
+        public bool Enable{get; set; }
     }
 }
